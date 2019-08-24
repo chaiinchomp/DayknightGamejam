8a86ec479727d50802abf3ffbc57b08fa8f89b44
chaiinchomp <chaiinchomp@users.noreply.github.com>
2019-08-24 15:11:45 -0700

    Add walls and collision logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private Rigidbody2D player;
 
     void Start() {
-        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        Vector3 newPos = player.transform.position;
+        Vector3 newPos = gameObject.transform.position;
         if (Input.GetButtonDown("Up")) {
-            newPos = player.transform.position + Vector3.up;
+            newPos = MoveIfPossible(gameObject.transform.position, Vector3.up);
         } else if (Input.GetButtonDown("Down")) {
-            newPos = player.transform.position + Vector3.down;
+            newPos = MoveIfPossible(gameObject.transform.position, Vector3.down);
         }
         if (Input.GetButtonDown("Left")) {
-            newPos = player.transform.position + Vector3.left;
+            newPos = MoveIfPossible(gameObject.transform.position, Vector3.left);
         } else if (Input.GetButtonDown("Right")) {
-            newPos = player.transform.position + Vector3.right;
+            newPos = MoveIfPossible(gameObject.transform.position, Vector3.right);
         }
 
-        player.transform.SetPositionAndRotation(newPos, Quaternion.identity);
+        gameObject.transform.SetPositionAndRotation(newPos, Quaternion.identity);
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate() {
     }
+
+    Vector3 MoveIfPossible(Vector3 currentPosition, Vector3 direction) {
+        Vector3 possiblePosition = currentPosition + direction;
+
+        Collider2D objAtDestination = Physics2D.OverlapPoint(new Vector2(possiblePosition.x, possiblePosition.y));
+        if (objAtDestination != null && objAtDestination.CompareTag("Wall")) {
+            return currentPosition;
+        }
+        return possiblePosition;
+    }
+
 }
