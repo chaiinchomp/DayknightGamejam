73059cbedf1c8c430a4efb74aaf198a9564f9ec1
chaiinchomp <chaiinchomp@users.noreply.github.com>
2019-08-24 14:16:03 -0700

    Initial commit
    Not much here just some prototyping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D player;
+
+    void Start() {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 newPos = player.transform.position;
+        if (Input.GetButtonDown("Up")) {
+            newPos = player.transform.position + Vector3.up;
+        } else if (Input.GetButtonDown("Down")) {
+            newPos = player.transform.position + Vector3.down;
+        }
+        if (Input.GetButtonDown("Left")) {
+            newPos = player.transform.position + Vector3.left;
+        } else if (Input.GetButtonDown("Right")) {
+            newPos = player.transform.position + Vector3.right;
+        }
+
+        player.transform.SetPositionAndRotation(newPos, Quaternion.identity);
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate() {
+    }
+}
